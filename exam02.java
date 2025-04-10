ca595d065ea3832c4132b9f604a986cb3e0c0049
nacoche cat <1661764235@qq.com>
2025-04-10 16:49:34 +0800

    abcd

---
+++ exam02.java
@@ -0,0 +1,19 @@
+//package com.data02;
+//
+//public class exam02 {
+//    public static void main(String[] args) {
+//        int x=6;
+//        if(x>1)
+//        {
+//            System.out.println("x>1");
+//        }else if(x<10)
+//        {
+//            System.out.println("x<10");
+//        }else if (x<29)
+//        {
+//            System.out.println("x<29");
+//        }else {
+//            System.out.println(" ");
+//        }
+//    }
+//}
